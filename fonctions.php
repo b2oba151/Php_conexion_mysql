8c429f637c3ad028a4ccdce5d2ff7048f314a709
b2oba151 <bodiarra20@gmail.com>
2023-03-06 15:16:33 +0100

    ajout dans fonctions.php :insertion_multiple

--- fonctions.php
+++ fonctions.php
@@ -56,7 +56,7 @@ function recuper_dernier_id(string $Nom, string $Prenom, string $Email, string $
     $conn = new PDO("mysql:host=$serveur;dbname=$base", $utilisateur, $motdepasse);
     // mettre le mode d'erreur PDO en exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // utiliser des guillemets simples pour les valeurs à insérer dans la requête SQL
+
     $sql = "INSERT INTO utilisateurs (nom, prenom, email, motdepasse) VALUES ('$Nom', '$Prenom', '$Email', '$Motdepasse')";
     // utiliser exec() car aucun résultat n'est renvoyé
     $conn->exec($sql);
@@ -67,6 +67,38 @@ function recuper_dernier_id(string $Nom, string $Prenom, string $Email, string $
   }
   $conn = null;
 }
-recuper_dernier_id('bouu', 'diarr', 'jdfhvf@gh.gh', 'dgfgfg');
+// recuper_dernier_id('bouu', 'diarr', 'jdfhvf@gh.gh', 'dgfgfg');
 ?>
 
+<?php
+function insertion_multiple(){
+  try {
+    require 'config_bd.php';
+    $conn = new PDO("mysql:host=$serveur;dbname=$base", $utilisateur, $motdepasse);
+    // mettre le mode d'erreur PDO en exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // commencer la transaction
+    $conn->beginTransaction();
+    // nos déclarations SQL
+    function element(string $Nom, string $Prenom, string $Email, string $Motdepasse){
+      $sql = "INSERT INTO utilisateurs (nom, prenom, email, motdepasse) VALUES ('$Nom', '$Prenom', '$Email', '$Motdepasse')";
+      return $sql;
+    };
+    // exécuter les requêtes SQL
+    $conn->exec(element('John', 'Doe', 'john@example.com', 'mdp1'));
+    $conn->exec(element('Mary', 'Moe', 'mary@example.com', 'mdp2'));
+    $conn->exec(element('Julie', 'Dooley', 'julie@example.com', 'mdp3'));
+    // valider la transaction
+    $conn->commit();
+    echo "Nouveaux enregistrements créés avec succès";
+  } catch(PDOException $e) {
+    // annuler la transaction si quelque chose a échoué
+    $conn->rollback();
+    echo "Erreur : " . $e->getMessage();
+  }
+  $conn = null;
+}
+
+insertion_multiple();
+?>
