e9332731c267849eda7baa243c5d15928f5272d7
b2oba151 <bodiarra20@gmail.com>
2023-03-07 03:16:10 +0100

    celui avant ne fonctione pas tres bien

--- fonctions.php
+++ fonctions.php
@@ -223,7 +223,7 @@
 
 
 <!-- parcourir_table -->
-           <?php require 'header_html.php' ?>
+      <?php require_once 'header_html.php' ?>
       
           <?php 
               function parcourir_table(string $table, array $champs){
@@ -279,61 +279,60 @@
                                   HTML;
                   }
 
-                  parcourir_table('utilisateurs',['id','nom','prenom','email','motdepasse'])
+                  //parcourir_table('utilisateurs',['id','nom','prenom','email','motdepasse'])
           ?>
 <!--  -->
 
 
 <!--  parcourir_table_where-->
-    <?php
-         function parcourir_table_where(string $table, array $champs,$chanp,mixed $condition, mixed $valeur){
-                afficherDebutTableau();
-            foreach($champs as $champ) {
-                echo "<th>$champ</th>";
-            }
-            echo "</tr>";
-            echo" </thead>";
+  <?php
+        function parcourir_table_where(string $table, array $champs, $champ, $condition, $valeur) {
+          afficherDebutTableau();
+          foreach ($champs as $champ) {
+              echo "<th>$champ</th>";
+          }
+          echo '</tr>
+                  </thead>';
 
-            class TableRows extends RecursiveIteratorIterator {
-                function __construct($it) {
-                parent::__construct($it, self::LEAVES_ONLY);
-                }
+          class TableRows extends RecursiveIteratorIterator {
+              function __construct($it) {
+                  parent::__construct($it, self::LEAVES_ONLY);
+              }
 
-                function current() {
-                return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-                }
+              function current() {
+                  return '<td style="width:150px;border:1px solid black;">' . parent::current(). '</td>';
+              }
 
-                function beginChildren() {
-                echo "<tr>";
-                }
+              function beginChildren() {
+                  echo '<tr>';
+              }
 
-                function endChildren() {
-                echo "</tr>" . "\n";
-                }
-            }
+              function endChildren() {
+                  echo '</tr>' . "\n";
+              }
+          }
 
-            try {
-                require 'config_bd.php';
-                $conn = new PDO("mysql:host=$serveur;dbname=$base", $utilisateur, $motdepasse);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          try {
+              require 'config_bd.php';
+              $conn = new PDO("mysql:host=$serveur;dbname=$base", $utilisateur, $motdepasse);
+              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                $champs_str = implode(",", $champs);
-                $stmt = $conn->prepare("SELECT $champs_str FROM $table  WHERE  ".$chanp."".$condition."".$valeur. "");
-                $stmt->execute();
+              $champs_str = implode(",", $champs);
+              $stmt = $conn->prepare("SELECT $champs_str FROM $table WHERE $champ $condition ?");
+              $stmt->execute([$valeur]);
 
-                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-                echo $v;
-                }
-            } catch(PDOException $e) {
-                echo "Error: " . $e->getMessage();
-            }
-            $conn = null;
-            echo "</table>";
-            }
+              $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+              foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                  echo $v;
+              }
+          } catch(PDOException $e) {
+              echo "Error: " . $e->getMessage();
+          }
+          $conn = null;
+          echo '</table></div>';
+          }
 
-
-            //parcourir_table_where('utilisateurs',['id','nom','prenom','email','motdepasse'],'id','>',"'18'")
+          //parcourir_table_where('utilisateurs', ['id', 'nom', 'prenom', 'email', 'motdepasse'], 'id', '>', '18');
     ?>
 <!--  -->
 
--- temp.php
+++ temp.php
@@ -1,4 +1,6 @@
-<?php 
- require 'header_html.php' ;
-?>
+<?php require 'header_html.php' ;?>
+<?php require 'fonctions.php' ;?>
 
+<?php parcourir_table_order_by('utilisateurs',['id','nom','prenom','email','motdepasse'],"'prenom'"); ?>
+
+<!-- <?php update_entre_where('utilisateurs', 'nom', 'nnn', 'id', '=', '26'); ?> -->
